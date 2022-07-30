7117362f5c5f115d3ff55cdaf2d094a2b3d11389
abril-bazann <abrilbazan28@gmail.com>
2022-07-30 20:15:34 -0300

    login, logout, perfil. mejoro el diseño

--- proyecto_montauk/app_montauk/urls.py
+++ proyecto_montauk/app_montauk/urls.py
@@ -1,7 +1,18 @@
 from unicodedata import name
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', inicio, name= 'inicio'),
+    path('about/', about, name= 'about'),
+    path('post/', post, name= 'post'),
+    path('contact/', contact, name= 'contact'),
+#-------------------
+    path('login/', login_request, name= 'login'),
+    path('register/', register, name= 'register'),
+    path('logout/', LogoutView.as_view(template_name="app_montauk/logout.html"), name= 'logout'),
+    path('editar_perfil/', editar_perfil, name= 'editar_perfil'),
+    path('agregar_avatar/', agregar_avatar, name= 'agregar_avatar'),
+    
 ]
--- proyecto_montauk/app_montauk/views.py
+++ proyecto_montauk/app_montauk/views.py
@@ -1,5 +1,97 @@
 from django.shortcuts import render
+import email
+from wsgiref.util import request_uri
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from app_montauk.models import Avatar
+from django.http import HttpResponse
+from django.template import Context, Template, loader
+from datetime import datetime
+import datetime
+from app_montauk.forms import UserRegisterForm, UserEditForm, AvatarForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def inicio(request):
-    return render(request, "app_montauk/index.html")
+    return render(request, "app_montauk/inicio.html")
+
+def about(request):
+    return render(request, "app_montauk/about.html")
+
+def post(request):
+    return render(request, "app_montauk/post.html")
+
+def contact(request):
+    return render(request, "app_montauk/contact.html")
+
+def login_request(request):
+    if request.method=="POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid:
+            client=request.POST ['username']
+            clave=request.POST ['password']
+
+            usuario= authenticate(username=client, password=clave)
+            if usuario is not None:
+                login(request, usuario)
+                return render(request, "app_montauk/inicio.html", {"mensaje":f"Te doy la bienvenida {usuario}"})
+            else:
+                return render(request, "app_montauk/inicio.html", {"mensaje":"Error; datos incorrectos"})
+        else:
+            return render(request, "app_montauk/inicio.html", {"mensaje":"Error; formulario erroneo"})
+    form= AuthenticationForm()
+    return render(request, "app_montauk/login.html", {"form":form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+
+            form.save()
+            return render(request, 'app_montauk/inicio.html', {'form':form,'mensaje':f"Usuario Creado:  {username}"})
+    else:
+        form = UserRegisterForm()
+    return render(request, 'app_montauk/registro.html', {'form': form})
+
+@login_required
+def editar_perfil(request):
+    usuario=request.user
+    #si es metodo post hago lo mismo que al agregar
+    if request.method == 'POST':
+        formulario=UserEditForm(request.POST, instance=usuario)
+        if formulario.is_valid():
+            informacion=formulario.cleaned_data
+            #datos que se modifican
+            usuario.email=informacion['email']
+            usuario.password1=informacion['password1']
+            usuario.password2=informacion['password2']
+            usuario.save()
+            return render(request, 'app_montauk/inicio.html', {'usuario':usuario, 'mensaje':'PERFIL EDITADO EXITOSAMENTE'})
+    #en caso de q no sea post
+    else:
+        #creo form con los datos que voy a modificar
+        formulario=UserEditForm(instance=usuario)
+    return render(request, 'app_montauk/editar_perfil.html', {'formulario':formulario, 'usuario':usuario.username})
+
+
+#saco uno y pongo otro
+def agregar_avatar(request):
+    if request.method == 'POST':
+        formulario=AvatarForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            avatar_viejo=Avatar.objects.get(user=request.user)
+            if(avatar_viejo.imagen):
+                avatar_viejo.delete()
+            avatar=Avatar(user=request.user, imagen=formulario.cleaned_data['imagen'])
+            avatar.save()
+            return render(request, 'app_montauk/inicio.html', {'usuario':request.user, 'mensaje':'AVATAR AGREGADO EXITOSAMENTE'})
+    else:
+        formulario=AvatarForm()
+    return render(request, 'app_montauk/agregar_avatar.html', {'formulario':formulario, 'usuario':request.user})
+
+
